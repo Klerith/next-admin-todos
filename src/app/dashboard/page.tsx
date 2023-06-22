9b15f05e52c18a896b301035f4fca67d3f2e9ded
Fernando Herrera <fernando.herrera85@gmail.com>
2023-06-22 09:50:27 -0400

    WidgetItem removed from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ export default function DashboardPage() {
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
 
 
-      <WidgetItem />
+      {/* <WidgetItem /> */}
       
 
     </div>
