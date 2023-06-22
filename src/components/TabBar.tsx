aed0fb7a2dec61d0cf0fc47939f6139eea9bc40d
Fernando Herrera <fernando.herrera85@gmail.com>
2023-06-22 09:58:41 -0400

    Length removed

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { setCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 // https://tailwindcomponents.com/component/radio-buttons-1
@@ -13,11 +14,13 @@ interface Props {
 
 export const TabBar = ({ tabOptions = [1,2,3,4], currentTab = 1 }: Props ) => {
 
+  const router = useRouter();
   const [selected, setSelected] = useState( currentTab );
 
   const onTabSelected = ( tab: number ) => {
     setSelected( tab );
     setCookie('selectedTab', tab.toString() );
+    router.refresh();
   }
 
   return (
