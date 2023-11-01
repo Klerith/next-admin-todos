f02b2bd6d9dc0a795e0f8bd9d10fcf7ed4cb51fe
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-01 16:24:31 -0400

    Actualizado a Next 14

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // experimental: {
+    // serverActions: true, // Ya no son experimentales desde la versión 14
+  // },
   images: {
     remotePatterns: [
       {
